1a2605943b6b6c35b679fe774571aaae0fa8c7b7
Tphilus <tkdadzie@st.ug.edu.gh>
2024-02-22 12:21:33 -0800

    feat: update

---
+++ react-ts-basics/src/App.tsx
@@ -0,0 +1,14 @@
+import CourseGoal from "./components/CourseGoal";
+
+function App() {
+  return (
+    <main>
+      <CourseGoal
+        title="LEarn React + TS"
+        description="Learn it fro the ground"
+      />
+    </main>
+  );
+}
+
+export default App;
