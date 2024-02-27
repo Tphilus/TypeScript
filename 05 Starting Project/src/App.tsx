496d9e4a460e4a840a83b5c6a4b0055e2337a649
Tphilus <tkdadzie@st.ug.edu.gh>
2024-02-26 23:14:10 -0800

    update

--- 05 Starting Project/src/App.tsx
+++ 05 Starting Project/src/App.tsx
@@ -1,16 +1,17 @@
-import AddTimer from './components/AddTimer.tsx';
-import Header from './components/Header.tsx';
-import Timers from './components/Timers.tsx';
+import AddTimer from "./components/AddTimer.tsx";
+import Header from "./components/Header.tsx";
+import Timers from "./components/Timers.tsx";
+import TimersContextProvider from "./store/timers-context.tsx";
 
 function App() {
   return (
-    <>
+    <TimersContextProvider>
       <Header />
       <main>
         <AddTimer />
         <Timers />
       </main>
-    </>
+    </TimersContextProvider>
   );
 }
 
